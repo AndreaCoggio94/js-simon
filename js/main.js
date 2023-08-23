04a6e3b74bd3c0932fdeb6a27830f2eb92b4d716
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-08-23 16:26:47 +0200

    tryed out how to count the milliseconds

--- js/main.js
+++ js/main.js
@@ -13,10 +13,11 @@ const minute = 1000 * 60;
 const hour = minute * 60;
 const day = hour * 24;
 
-// get date of today
-const todayDate = new Date();
 // get date of the revision
 const revisionDate = new Date("August 24, 2023 09:30:00");
+
+// get date of today
+const todayDate = new Date();
 
 let msRevision = revisionDate.getTime();
 console.log(msRevision);
@@ -27,8 +28,32 @@ console.log("ora è così " + msNow);
 let timeLeft = msRevision - msNow;
 console.log(timeLeft);
 
-let doubleCheck = new Date(msNow);
-console.log(doubleCheck);
+let timerDays = parseInt(timeLeft / day);
+timeLeft = timeLeft - timerDays;
+console.log(timerDays);
+console.log(timeLeft);
+
+let timerHours = parseInt(timeLeft / hour);
+timeLeft = timeLeft - timerHours * hour;
+console.log(timerHours);
+console.log(timeLeft);
+
+let timerMinutes = parseInt(timeLeft / minute);
+timeLeft = timeLeft - timerMinutes * minute;
+console.log(timerMinutes);
+console.log(timeLeft);
+
+let timerSeconds = parseInt(timeLeft / second);
+timeLeft = timeLeft - timerSeconds * second;
+console.log(timerSeconds);
+console.log(timeLeft);
+
+// function counterTimeLeft() {
+
+// }
+
+// let doubleCheck = new Date(msNow);
+// console.log(doubleCheck);
 
 // !! to create the actual countdown
 
